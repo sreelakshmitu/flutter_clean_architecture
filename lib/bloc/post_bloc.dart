42e876d3e01380ffd5fe035f95047cdcaf641858
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-25 17:45:00 +0900

    Added : emit state

--- lib/bloc/post_bloc.dart
+++ lib/bloc/post_bloc.dart
@@ -2,16 +2,21 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:clean_architecture/bloc/post_events.dart';
 import 'package:clean_architecture/bloc/post_state.dart';
+import 'package:clean_architecture/data/post_repository.dart';
+import 'package:clean_architecture/models/post.dart';
 
 
 class PostBloc extends Bloc<PostEvent,PostState>{
   PostBloc():super(PostInitialFetchState()){
     on<PostFetchEvent>(postfetchevent);
+  
   }
   
 
-  FutureOr<void> postfetchevent(PostFetchEvent event, Emitter<PostState> emit) {
-    emit(PostInitialFetchState());
+  FutureOr<void> postfetchevent(PostFetchEvent event, Emitter<PostState> emit) async{
+    emit(PostFetchLoadingState());
+    List<Post>post= await PostRepo.getposts();
+    emit(PostFetchSuccessfulState(data:post));
   }
 }
 
