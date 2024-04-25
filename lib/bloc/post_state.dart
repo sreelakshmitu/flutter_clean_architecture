085ba6f8de9548cc04f9e1b46677313af4e99d39
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-25 17:45:23 +0900

    Added : parameters to post state

--- lib/bloc/post_state.dart
+++ lib/bloc/post_state.dart
@@ -1,7 +1,16 @@
+import 'package:clean_architecture/models/post.dart';
+
 abstract class PostState{}
 
 class PostInitialFetchState extends PostState{}
 
-class PostFetchSuccessfulState extends PostState{}
+class PostFetchLoadingState extends PostState{}
+
+class PostFetchSuccessfulState extends PostState{
+  
+  final List<Post> data;
+
+  PostFetchSuccessfulState({required this.data});
+}
 
 class PostFetchErrorState extends PostState{}
